693bf57189d9f08ec368daae565696f89030d66b
kolbukm <mkolbuk@pl.sii.eu>
2018-01-05 23:30:00 +0100

    Initial Project

---
+++ eFlorist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eFlorist.MvcApplication" Language="C#" %>
---
+++ eFlorist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eFlorist.Startup))]
+namespace eFlorist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
