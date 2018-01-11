acc2ac004d8a21aaf63dada0076727f08de508b0
kolbukm <mkolbuk@pl.sii.eu>
2018-01-11 16:21:42 +0100

    florist list

--- eFlorist/Models/Florist.cs
+++ eFlorist/Models/Florist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace eFlorist.Models
     public class Florist
     {
         public int Id { get; set; }
+        [Display(Name = "Nazwa kwiaciarni")]
         public string FloristName { get; set; }
         public virtual ICollection<Item> ItemsList { get; set; }
         public virtual ICollection<Invoice> InvoiceList { get; set; }
