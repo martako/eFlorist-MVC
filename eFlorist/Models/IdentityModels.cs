27c9c039bd25d3efce1f980cf46d8fa2f895a4ab
martako <martako>
2018-01-14 21:23:29 +0100

    two contexts

--- eFlorist/Models/IdentityModels.cs
+++ eFlorist/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace eFlorist.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFloristDbContext", throwIfV1Schema: false)//DefaultConnection
         {
         }
 
